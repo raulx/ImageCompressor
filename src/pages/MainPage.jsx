b6351e8a3d1c7f1fbb4131b246557eca5a42d0bd
Raulx103 <raulx103@gmail.com>
2024-02-13 10:13:11 +0530

    file selecting mechanism added

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,32 @@
+import { useState } from "react";
+
 function MainPage() {
+  const [fileSelected, setFileSelected] = useState({
+    name: "",
+    file: "",
+    size: 0,
+    imgLocation: "",
+  });
+
+  const handleImageSelect = (e) => {
+    const file = e.target.files[0];
+    const fileName = e.target.value.split("\\").pop();
+    const fileSize = (file.size / 1024).toFixed(2) + "kb";
+
+    const img = URL.createObjectURL(file);
+    setFileSelected((prevValue) => {
+      return {
+        ...prevValue,
+        name: fileName,
+        size: fileSize,
+        imgLocation: img,
+        file: file,
+      };
+    });
+  };
+
   return (
-    <div className="md:w-2/3 mx-auto py-2 flex flex-col gap-6">
+    <div className="md:w-2/3 mx-auto py-2 flex flex-col overflow-hidden gap-6">
       <div className="flex justify-center gap-6 items-center">
         <label
           htmlFor="image-file"
@@ -8,17 +34,27 @@ function MainPage() {
         >
           Choose File
         </label>
-        <input id="image-file" type="file" className="hidden" />
+        <input
+          id="image-file"
+          type="file"
+          accept="image/*"
+          className="hidden"
+          onChange={(e) => {
+            handleImageSelect(e);
+          }}
+        />
         <span className="text-3xl text-center">:</span>
-        <p className=" text-dark text-lg font-bold">No File Selected.</p>
+        <p className="text-dark text-lg font-bold">{fileSelected.name}</p>
       </div>
       <div className="flex flex-col justify-center items-center gap-10">
         <div className="w-72 h-56 border-black border-2 rounded-lg">
           <img
-            src="https://images.unsplash.com/photo-1585995603666-5bd6b348de9d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZmFrZXxlbnwwfHwwfHx8MA%3D%3D"
+            src={fileSelected.imgLocation}
             className="h-full w-full object-contain"
           />
-          <p className="font-bold text-light py-2">Original Image.</p>
+          <p className="font-bold text-light py-2">
+            Original Image.{fileSelected.size}
+          </p>
         </div>
         <div className="w-72 h-56 border-black border-2 rounded-lg">
           <img
@@ -27,6 +63,56 @@ function MainPage() {
           />
           <p className="font-bold text-light py-2">Ai Enchanced Image.</p>
         </div>
+      </div>
+      <div className="w-72 mt-4 mx-auto">
+        <div className="flex justify-between">
+          <p>Keep :</p>
+          <div>
+            <input id="original" type="radio" />
+            <label>Original</label>
+          </div>
+          <div>
+            <input id="aienhanced" type="radio" />
+            <label>Ai Enhanced</label>
+          </div>
+        </div>
+        <div>
+          <div>
+            <input type="radio" />
+            <label>Less than 50kb</label>
+          </div>
+          <div>
+            <input type="radio" />
+            <label>Between 50kb-100kb</label>
+          </div>{" "}
+          <div>
+            <input type="radio" />
+            <label>Between than 100kb - 300kb</label>
+          </div>{" "}
+          <div>
+            <input type="radio" />
+            <label>Between 300kb - 1MB</label>
+          </div>{" "}
+          <div>
+            <input type="radio" />
+            <label>Between 1Mb-5Mb</label>
+          </div>
+          <button className="px-4 py-2 bg-dark text-white rounded-lg">
+            Compress
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center gap-10">
+        <div className="w-72 h-56 border-black border-2 rounded-lg">
+          <img
+            src="https://images.unsplash.com/photo-1585995603666-5bd6b348de9d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZmFrZXxlbnwwfHwwfHx8MA%3D%3D"
+            className="h-full w-full object-contain"
+          />
+          <p className="font-bold text-light py-2">Final Image.</p>
+        </div>
+        <button className="py-1 px-2 bg-dark text-white rounded-md">
+          Save
+        </button>
       </div>
     </div>
   );
